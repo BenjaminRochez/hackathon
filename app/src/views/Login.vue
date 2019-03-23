d98f6746fb67a1e046e67091a6e07aa77c5d7179
Benjamin Rochez <rochez.benjamin@gmail.com>
2019-03-23 03:05:12 +0100

    :lipstick: Update login page UI

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,43 +1,39 @@
 <template>
-  <div class="login">
-    <v-layout>
+  <div class="login fill-height">
+    <v-layout fill-height>
       <v-flex xs6>
-          <div class="pr-relative">
-            <v-img class="pr-login__illu" :src="require('../assets/login.jpg')"></v-img>
+        <div class="relative">
+          <v-img class="pr-login__illu" :src="require('../assets/login.jpg')"></v-img>
         </div>
       </v-flex>
-      <v-flex xs6>
-        <div class="pr-login__form">
-          <div class="oa-login__form-wrapper text-md-left">
-            <div class="oa-login__header">
-              <router-link class="oa-login__logo" to="/">
-                <h1>
-                  <span>Proxy</span>
-                  <span class="font-weight-light">Relay</span>
-                </h1>
-              </router-link>
-            </div>
-            <v-form ref="form">
-              <v-text-field label="Email adress" append-icon="alternate_email" class="mb-4"></v-text-field>
-              <v-text-field
-                
-                label="Password"
-                append-icon="visibility"
-                
-                class="mb-5"
-              ></v-text-field>
+      <v-flex xs6 fill-height>
+        <div class="relative fill-height">
+          <div class="pr-login__form">
+            <div class="oa-login__form-wrapper text-md-left">
+              <div class="oa-login__header">
+                <router-link class="oa-login__logo" to="/">
+                  <h1>
+                    <span>Proxy</span>
+                    <span class="font-weight-light">Relay</span>
+                  </h1>
+                </router-link>
+              </div>
+              <v-form ref="form">
+                <v-text-field label="Email adress" append-icon="alternate_email" class="mb-4"></v-text-field>
+                <v-text-field label="Password" append-icon="visibility" class="mb-1"></v-text-field>
 
-              <v-btn block class="primary v-btn--xl">Login</v-btn>
+                <v-btn block class="primary large">Login</v-btn>
 
-              <p class="mt-5">
-                Need an account ?
-                <a href="#">Create an account</a>
-              </p>
-            </v-form>
-            <div class="pr-login__or">OR</div>
-            <div class="pr-login__social">
-              <v-btn class="pr-facebbok">Facebook</v-btn>
-              <v-btn class="pr-google">Google</v-btn>
+                <p class="mt-1">
+                  Need an account ?
+                  <a href="#">Create an account</a>
+                </p>
+              </v-form>
+              <div class="pr-login__or mb-2">OR</div>
+              <div class="pr-login__social">
+                <button class="loginBtn loginBtn--facebook">Login with Facebook</button>
+                <button class="loginBtn loginBtn--google">Login with Google</button>
+              </div>
             </div>
           </div>
         </div>
@@ -47,21 +43,50 @@
 </template>
 
 <script>
-export default {
-};
+export default {};
 </script>
 
 <style>
-    .pr-login__illu{
-        width: 100%;
-        height: 100vh;
-        position: absolute;
-        left: 0;
-        top: 0;
-    }
-    .pr-relative{
-        position: relative;
-    }
+.pr-login__illu {
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+.relative {
+  position: relative;
+}
+.pr-login__social, .pr-login__or{
+    text-align: center;
+}
+
+.pr-login__or:before, .pr-login__or:after{
+    content: '';
+    width: 50px;
+    height: 1px;
+    content: '';
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+    background: rgba(0,0,0,0.5);
+}
+
+.pr-login__or:before{
+    margin-right: 10px;
+}
+
+.pr-login__or:after{
+    margin-left: 10px;
+}
+.pr-login__form {
+  position: absolute;
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 100%;
+  padding: 0 150px;
+}
 </style>
 
 
--- app/src/views/LoginSelector.vue
+++ app/src/views/LoginSelector.vue
@@ -1,55 +1,54 @@
 <template>
   <div class="pr-selector">
-      <v-container align-content-center grid-list-xl>
-    <v-layout>
-        
-      <v-flex xs6>
-        <v-card>
-          <v-card-title primary-title class="text-sm-center" >
-            
-              <h3 class="headline mb-0 pr-headline">Kangaroo Valley Safari</h3>
-              <div>{{ card_text }}</div>
-
-          </v-card-title>
-          <v-container>
-            <v-layout>
-              <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"></v-img>
-            </v-layout>
-          </v-container>
-        </v-card>
-      </v-flex>
-
-
-      <v-flex xs6>
-        <v-card>
-          <v-card-title primary-title class="text-sm-center" >
-            
-              <h3 class="headline mb-0 pr-headline">Kangaroo Valley Safari</h3>
-              <div>{{ card_text }}</div>
-
-          </v-card-title>
-          <v-container>
-            <v-layout>
-              <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"></v-img>
-            </v-layout>
-          </v-container>
-        </v-card>
-      </v-flex>
-      
-    </v-layout>
+    <v-container align-content-center grid-list-xl fill-height >
+      <v-layout align-center row wrap>
+        <v-flex xs6 v-for="selector in selectors" :key="selector.user">
+          <v-card>
+            <v-card-title primary-title class="text-sm-center">
+              <h3 class="headline mb-0 pr-headline">{{selector.user}}</h3>
+            </v-card-title>
+            <v-container>
+              <v-layout>
+                <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"></v-img>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      selectors: [
+        {
+          user: "Vendeur"
+        },
+        {
+          user: "Acheteur"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 
 <style>
+.pr-headline {
+  width: 100%;
+}
 
-.pr-headline{
-    width: 100%;
+.pr-selector {
+    height: 100vh;/* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#ffd38c+0,ffa509+100 */
+background: #ffd38c; /* Old browsers */
+background: -moz-linear-gradient(-45deg, #ffd38c 0%, #ffa509 100%); /* FF3.6-15 */
+background: -webkit-linear-gradient(-45deg, #ffd38c 0%,#ffa509 100%); /* Chrome10-25,Safari5.1-6 */
+background: linear-gradient(135deg, #ffd38c 0%,#ffa509 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffd38c', endColorstr='#ffa509',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
 }
 </style>
 
